c90e93b634dff632acdb7b8ac60e14f44b657b11
Akintola Rahmat <akintolarahmah@gmail.com>
2024-01-14 05:04:57 +0000

    Added state management to create note
    Signed-off-by: Akintola Rahmat <akintolarahmah@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import React, {useState} from 'react';
 import './App.css';
 import TextField from './components/TextField';
 
+
+
 const App:React.FC = () => {
-  const [note, setNote] = useState<string>("")
   return (
     <div className="App">
       <span className="App-header">Notefy</span>
-      <TextField note={note} setNote={setNote} />
+      <TextField  />
       
     </div>
   );
--- src/components/TextField.tsx
+++ src/components/TextField.tsx
@@ -1,19 +1,36 @@
-import React from 'react'
+import React,{useState}from 'react';
 import './styles.css';
 
-interface Props {
-  note: string;
-  setNote: React.Dispatch<React.SetStateAction<string>>
+interface Note {
+  title: string;
+  content: string;
 }
 
-const TextField = ({note, setNote}: Props) => {
+const initialValues: Note = {
+  title: "",
+  content: "",
+};
+
+const TextField: React.FC = () => {
+  const [note, setNote] = useState<Note>(initialValues)
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = event.target;
+    setNote((prevNote) => ({ ...prevNote, [name]: value }));
+  };
+
+
   return (
-    <form className='text'>
-        <div className='text_input'>
+    <form className='text flex flex-col justify-between items-center'>
+      <div className='text_input text_note'>
+        <label htmlFor='title'>Note title:</label>
+        <input id='title' type='text' name='title' placeholder='Enter note title' onChange={handleChange} />
+      </div>
+      <div className='text_note'>
         <label htmlFor='addname' className='text_label'>Add a new note</label>
-        <textarea id='addnote' name='addnote' rows={7} cols={70} className='text_box'></textarea>
-        </div>
-        <button type='submit' className='text_submit'>Save</button>
+        <textarea id='addnote' name='content' rows={7} cols={70} className='text_box' onChange={handleChange}></textarea>
+      </div>
+      <button type='submit' className='text_submit'>Save</button>
     </form>
   )
 }
