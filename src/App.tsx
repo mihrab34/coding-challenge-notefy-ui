05cb09f2e444dc1ac7d517575f988369fc3bb444
Akintola Rahmat <akintolarahmah@gmail.com>
2024-01-16 19:25:05 +0000

    Added Api integration to create note

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React, {useState} from 'react';
 import './App.css';
+import { INote } from './components/model';
 import TextField from './components/TextField';
 
-
-
 const App:React.FC = () => {
+  const [notes, setNotes] = useState<INote[]>([])
   return (
     <div className="App">
       <span className="App-header">Notefy</span>
--- src/components/TextField.tsx
+++ src/components/TextField.tsx
@@ -1,34 +1,65 @@
 import React,{useState}from 'react';
 import './styles.css';
+import { INote } from './model';
 
-interface Note {
-  title: string;
-  content: string;
-}
+// interface Note {
+//   title: string;
+//   content: string;
+// }
 
-const initialValues: Note = {
+const initialValues: INote = {
   title: "",
-  content: "",
+  body: "",
 };
 
-const TextField: React.FC = () => {
-  const [note, setNote] = useState<Note>(initialValues)
+const TextField = () => {
+  const [note, setNote] = useState<INote>(initialValues)
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    const { name, value } = event.target;
-    setNote((prevNote) => ({ ...prevNote, [name]: value }));
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const value = e.target.value;
+    setNote({ ...note, [e.target.id]: value });
   };
+
+  const handleSubmit = async(e:React.FormEvent) => {
+    e.preventDefault();
+    if (note) {
+      let options = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(note)
+    };
+
+    try {
+        const response = await fetch("http://localhost:5000/api/", options);
+        const data = await response.json();
+        if (response.status === 201) {
+          alert(data.message);
+          setNote(initialValues)
+        }
+        else if (response.status === 400){
+          setNote({...note, [note.title]:" "});
+          alert("Note title must be unique");
+        }
+      } catch (error) {
+        alert(error);
+      }
+    }
+ };
+
+ const {title, body} = note;
 
 
   return (
-    <form className='text flex flex-col justify-between items-center'>
+    <form className='text flex flex-col justify-between items-center' onSubmit={handleSubmit}>
       <div className='text_input text_note'>
         <label htmlFor='title'>Note title:</label>
-        <input id='title' type='text' name='title' placeholder='Enter note title' onChange={handleChange} />
+        <input id='title' type='text' name='title' placeholder='Enter note title' value={title} onChange={handleChange} />
       </div>
       <div className='text_note'>
         <label htmlFor='addname' className='text_label'>Add a new note</label>
-        <textarea id='addnote' name='content' rows={7} cols={70} className='text_box' onChange={handleChange}></textarea>
+        <textarea id='body' name='body' rows={7} cols={70} className='text_box' value={body} onChange={handleChange}></textarea>
       </div>
       <button type='submit' className='text_submit'>Save</button>
     </form>
